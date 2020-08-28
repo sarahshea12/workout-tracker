7221ccf9176b3d3de01eb72143aa703133c9cdf6
Sarah Shealy <sshealy124@gmail.com>
2020-08-28 16:11:12 -0400

    added develop folder'

---
+++ Develop/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));

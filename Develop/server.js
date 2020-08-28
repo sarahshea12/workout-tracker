5442c2f95b693c3943b311a4241cc012e4af98a6
Sarah Shealy <sshealy124@gmail.com>
2020-08-28 16:48:21 -0400

    new structure

--- Develop/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const mongojs = require("mongojs");
-const logger = require("morgan");
-const app = express();
-
-app.use(logger("dev"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
